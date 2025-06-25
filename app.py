04efb3c0837656e6ddd16832e070b9029aa15e6c
Reshma <reshmaphilip12@gmail.com>
2025-06-25 18:30:25 +0530

    Final_Version

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 from flask import Flask
+from flask import render_template, request
+import pickle
+import numpy as np
+from os import path
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template("home.html")
+
+
+@app.route("/result", methods=["POST"])
+def result_page():
+    cntry = int(request.form["cntry"])
+    b_s = int(request.form["b_s"])
+    s_s = int(request.form["s_s"])
+    w_s = int(request.form["w_s"])
+    cntnt = int(request.form["cntnt"])
+
+    file_name = "model.pkl"
+    with open(path.join("static", file_name), "rb") as f:
+        lr_model = pickle.load(f)
+
+    pred = lr_model.predict(np.array([[cntry, b_s, s_s, w_s, cntnt]]))[0]
+
+    return render_template("result.html", prediction=pred)
